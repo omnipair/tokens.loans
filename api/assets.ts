e51dcec941b614f2103ec7520419723ae5b029fd
Muhammed <muhammed@omnipair.fi>
2026-05-07 06:45:28 +0800

    Add SEO previews and support Vercel KV Redis envs

--- api/assets.ts
+++ api/assets.ts
@@ -74,7 +74,9 @@ function isLocalDevelopment() {
 }
 
 function isRedisConfigured() {
-  return Boolean(process.env.UPSTASH_REDIS_REST_URL && process.env.UPSTASH_REDIS_REST_TOKEN);
+  const redisUrl = process.env.UPSTASH_REDIS_REST_URL ?? process.env.KV_REST_API_URL;
+  const redisToken = process.env.UPSTASH_REDIS_REST_TOKEN ?? process.env.KV_REST_API_TOKEN;
+  return Boolean(redisUrl && redisToken);
 }
 
 function getRedis() {
